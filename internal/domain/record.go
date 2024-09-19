0346e15cf9e00511fe5f4765e012e46d490dc424
joseph.davis <joseph.davis@thinsurance.de>
2024-09-19 19:37:56 +0200

    added meta data

--- internal/domain/record.go
+++ internal/domain/record.go
@@ -9,16 +9,16 @@ type Record struct {
 	Timestamp     time.Time         `json:"timestamp"`
 	Level         Level             `json:"level"`
 	Message       string            `json:"message"`
-	Tags          map[string]string `json:"tags,omitempty"`
+	MetaData      map[string]string `json:"meta_data,omitempty"`
 	TransactionID string            `json:"transaction_id,omitempty"`
 }
 
-func NewRecord(level Level, message string, tags map[string]string, transactionID string) *Record {
+func NewRecord(level Level, message string, metaData map[string]string, transactionID string) *Record {
 	return &Record{
 		Timestamp:     time.Now(),
 		Level:         level,
 		Message:       message,
-		Tags:          tags,
+		MetaData:      metaData,
 		TransactionID: transactionID,
 	}
 }
