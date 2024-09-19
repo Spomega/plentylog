06afeeb849952b9d15177bd6531cf5b154b824bd
joseph.davis <joseph.davis@thinsurance.de>
2024-09-19 19:57:29 +0200

    added console_driver.go

--- internal/infrastructure/console_driver.go
+++ internal/infrastructure/console_driver.go
@@ -1,1 +1,29 @@
 package infrastructure
+
+import (
+	"fmt"
+	log "plentylog/internal/domain"
+)
+
+type ConsoleDriver struct{}
+
+// WriteLog writes a log record to the console.
+func (c *ConsoleDriver) WriteLog(record *log.Record) error {
+	fmt.Printf("[%s] %s: %s", record.Timestamp.Format("2006-01-02T15:04:05Z07:00"), record.Level.String(), record.Message)
+
+	if record.TransactionID != "" {
+		fmt.Printf(" [TransactionID: %s]", record.TransactionID)
+
+	}
+
+	if len(record.MetaData) > 0 {
+		fmt.Printf("[Attributes]:")
+		for key, value := range record.MetaData {
+			fmt.Printf("%s:%s n", key, value)
+		}
+	}
+
+	fmt.Println()
+
+	return nil
+}
